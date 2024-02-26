10ca371707407dbd1359dd7b0805cec652f25c32
nadirbashir <79132799+nadirbashir@users.noreply.github.com>
2024-02-26 15:58:46 +0500

    add class notes

--- Javascript class 01/app.js
+++ Javascript class 01/app.js
@@ -1,5 +1,12 @@
 alert('Hello Javascript');
 
+/* Primitive Data Types
+ Number
+ String
+ Boolean
+ Null
+ Undefined
+*/
 
 // var my foo //illegal cannot use all special characters except $ and _
 // var my.foo //illegal
@@ -8,12 +15,29 @@ alert('Hello Javascript');
 var $foo // legal  
 var _foo  //legal
 // var var = myFoo
-var guyWhoKeepsSayingHisOwnName = 'Salman' // camel case best practice
-var guy_who_keeps_saying_his_own_name = 'Salman' // snake case best practice
+var guyWhoKeepsSayingHisOwnName = 'Salman'; // camel case best practice
+var guy_who_keeps_saying_his_own_name = 'Ahmed'; // snake case best practice
 
 console.log(guyWhoKeepsSayingHisOwnName);
-var sum = 4 + 3.87348
+var sum = 4 + 3.87348;
 
-alert(sum + "Sum of two numbers");
+// Basic arithmetic operations
 
-console.log("hello")
+// var value = 5;
+
+// var sub = value - 2;
+
+// var mult = value * 3;
+
+// var division = value + 4/ 2; 
+
+// var ans = (value + 4)/ 2; 
+
+// var exponentiation = value ** 2 
+
+alert(sum + " Sum of two numbers"); 
+sum = sum + ' converted to string' // string concatination number + string type will be string
+
+console.log("hello");
+
+console.log(typeof(sum));
