0b2f534d6b8e271cb648f1d9cfc5e4ad148793d3
nadirbashir <79132799+nadirbashir@users.noreply.github.com>
2024-02-26 15:45:57 +0500

    javascript class 01

---
+++ Javascript class 01/app.js
@@ -0,0 +1,19 @@
+alert('Hello Javascript');
+
+
+// var my foo //illegal cannot use all special characters except $ and _
+// var my.foo //illegal
+// var 2myfoo //illegal cannot start with number
+// var var = 'hello' // illegal cannot use reserved keyword
+var $foo // legal  
+var _foo  //legal
+// var var = myFoo
+var guyWhoKeepsSayingHisOwnName = 'Salman' // camel case best practice
+var guy_who_keeps_saying_his_own_name = 'Salman' // snake case best practice
+
+console.log(guyWhoKeepsSayingHisOwnName);
+var sum = 4 + 3.87348
+
+alert(sum + "Sum of two numbers");
+
+console.log("hello")
