5dac7e39488ed9c5261549b571a16ebbc612eafe
nadirbashir <79132799+nadirbashir@users.noreply.github.com>
2024-02-28 13:33:13 +0500

    class code javascript basic operators

---
+++ unfamiliar operators | string concatination | prompts/app.js
@@ -0,0 +1,30 @@
+// var num = 6;
+// console.log(num); // 6
+// num++; // 6  post increment
+// console.log(num); //7
+// console.log(num++); // 7
+// console.log(num); // 8
+// ++num; // 9 pre increment
+// console.log(num); // 9
+// console.log(++num);  // 10
+
+// practice decrement on your own
+// post decrement num--
+// pre decrement --num
+
+
+// var number = 6;
+// number = number + 5;
+// number += 5; // same as number = number + 5
+// number -= 5; // same as number = number - 5
+// number *= 5;
+// number /= 5;
+
+// 5  + '5' result will be 55  plus operator with string operands always concatination
+
+// 5 * '5' //results 25
+// 5 * '' //results 0;
+
+var val1 = +prompt('Enter Value'); // add + to convert String to  number if possible other wise results NaN
+var val2 = +prompt('Enter Value');
+console.log(val1 + val2);
