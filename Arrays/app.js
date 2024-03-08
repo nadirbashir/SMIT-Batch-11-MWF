b24e2a62fdab9db7a66968c3fc2e10ed41bca536
nadirbashir <79132799+nadirbashir@users.noreply.github.com>
2024-03-08 21:13:54 +0500

    class code arrays

---
+++ Arrays/app.js
@@ -0,0 +1,27 @@
+// var arr = [];
+// var weekDays = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+// console.log(weekDays[4]);
+// var countries = ["pakistan", "china", "Turkey", "japan"];
+// countries[2] = "Turkiye";
+// console.log(countries);
+// var mixedValues = [1, "foo", 4, null, false, undefined];
+
+// console.log(weekDays[55]);
+
+// arr[3] = true;
+// console.log(arr); // [undefined, undefined, undefined, true];
+// var arr1 = ["a", "b"];
+// var arr2 = ["a", "b"];
+// console.log(arr1 === arr2);
+
+// console.log(arr1[0] === arr2[0]);
+
+var fruits = ["apple", "mango"];
+
+fruits.push("Kiwi");
+fruits.push("grapes", "banana", "orange");
+fruits.pop();
+var deletedElement = fruits.pop();
+console.log(deletedElement);
+// console.log(fruits.length)
+console.log(fruits);
